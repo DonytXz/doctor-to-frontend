45ef9784c4661f4d8707b77822d1c6afaecc7438
Carlos Donato Alvarez Farias <0318120621@miutt.edu.mx>
2023-08-29 14:38:18 -0600

    [ADDED] using headers id & token

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 // import { redirect } from 'next/navigation';
-const API = "http://localhost:3000";
+// const API = "http://localhost:3000";
+const API = process.env.NEXT_PUBLIC_API_URL;
 // import { signIn, signOut } from "next-auth/react";
 
 export const login = (values: any) => {
+  console.log(API, "api");
+  
   axios
     .post(`${API}/usc/cosmotologist/login`, { email: values.username, password: values.password })
     .then(function (response:any) {
-      localStorage.setItem("token", response?.token);
-      localStorage.setItem("id", response?.id);
+      localStorage.setItem("token", response?.data?.token);
+      localStorage.setItem("id", response?.data?.id);
       // redirect('/');
       window.location = "/" as any;
       console.log(response);
